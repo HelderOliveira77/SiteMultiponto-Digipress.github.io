83ec71eb191f216e3c4023153ecbfa289ffdaeeb
Helder <testeanalytics77@gmail.com>
2025-02-12 09:56:51 +0000

    Atualizar Site para repositório remoto

--- js/scripts.js
+++ js/scripts.js
@@ -12,4 +12,9 @@ button.addEventListener('click', function () {
 
 
 
-
+// CARROSSEL MULTIPONTO
+/*!
+  * Bootstrap v4.5.0 (https://getbootstrap.com/)
+  * Copyright 2011-2020 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
+  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+  */
